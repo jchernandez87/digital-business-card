e2e446560afce978d4c3017262c0e06074984ee8
Juan Carlos <jchernandez827@gmail.com>
2022-06-02 18:59:36 -0500

    Add footer

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,14 @@
 import profile from '../images/profile-img.png';
 import '../styles/Card.css';
 import Main from './Main';
+import Footer from './Footer';
 
 function Card() {
   return (
     <div className="card-container">
       <img className="profile-pic" src={profile} alt="profile-pic" />
       <Main />
+      <Footer />
     </div>
   );
 }
