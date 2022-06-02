cfabe9660f928faf2028c86b2b1045fd151646d5
Juan Carlos <jchernandez827@gmail.com>
2022-06-02 15:01:44 -0500

    React setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Digital Business Card Project
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
